09385ee31717e278722c5809e35d50bd481c27ee
macantru <macantillo72@gmail.com>
2021-01-06 20:47:23 -0500

    adding contact page and express app

---
+++ node-server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser'); 
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+/**
+ * req -> Request - Peticion😂
+ * res -> Response - Respuesta
+ */
+
+app.get('/', function(peticion, respuesta)  {
+    const {body, query, params, headers} = peticion;
+    respuesta.json({body, params, query, headers});
+})
+app.post('/', function(peticion, respuesta)  {
+    const {body, query, params, headers} = peticion;
+    respuesta.json({body, params, query, headers});
+})
+
+app.listen(3000);
+console.log("Aplicacion corriendo en el puerto 3000");
+
