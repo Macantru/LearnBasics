d6f368c0c2f5b492ed9873cb4f06ace593c298d3
macantru <macantillo72@gmail.com>
2021-02-18 20:31:58 -0500

    to do list

--- To-Do-List/index.js
+++ To-Do-List/index.js
@@ -4,11 +4,14 @@ const list = document.getElementById("list");
 const text = document.getElementById("text");
 let newItem = "";
 
+// eslint-disable-next-line no-unused-vars
 function darElValor(valor) {
   newItem = valor;
 }
 
+
 function createBtn(listItem) {
+  console.log(listItem)
   const btn = document.createElement("button");
   btn.innerHTML = "x";
   btn.classList.add("buttonX");
@@ -20,14 +23,17 @@ function createBtn(listItem) {
 
 function addStyle(element) {
   element.classList.add("crossOut");
+  element.innerHTML =  `<i class="fa fa-check"></i>` + element.innerHTML 
 }
 
 function removeStyle(element) {
   element.classList.remove("crossOut");
+  const icons = element.getElementsByClassName('fa-check')
+  icons[0].remove()
+  
 }
 
 let isCrossOut = false
-
 function toggleStyle(param) {
   if (isCrossOut) {
     removeStyle(param)
@@ -36,8 +42,11 @@ function toggleStyle(param) {
     addStyle(param)
     isCrossOut = true
   }
+
 }
 
+
+// eslint-disable-next-line no-unused-vars
 function insertListItem() {
   if (!newItem) return alert("¡Invalid text!");
   // Create list item and add current value from input
@@ -45,21 +54,18 @@ function insertListItem() {
   const span = document.createElement("span")
   span.innerText= newItem;
 
-  li.innerHTML = `<i class="fa fa-check"></i>` 
-
   li.append(span);
   // Create and add button to list item
   createBtn(li);
   list.append(li);
-
-
+ 
   // Reset values
   text.value = "";
   newItem = "";
 
   li.addEventListener("click", () => toggleStyle(li))
+
 }
 
 
 
- 
