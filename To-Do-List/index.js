3de9277a1265e54ae92654c4947cf115d822a7ff
macantru <macantillo72@gmail.com>
2021-02-22 22:10:31 -0500

    finished to do list

--- To-Do-List/index.js
+++ To-Do-List/index.js
@@ -9,9 +9,8 @@ function darElValor(valor) {
   newItem = valor;
 }
 
-
 function createBtn(listItem) {
-  console.log(listItem)
+  console.log(listItem);
   const btn = document.createElement("button");
   btn.innerHTML = "x";
   btn.classList.add("buttonX");
@@ -21,51 +20,34 @@ function createBtn(listItem) {
   listItem.append(btn);
 }
 
-function addStyle(element) {
-  element.classList.add("crossOut");
-  element.innerHTML =  `<i class="fa fa-check"></i>` + element.innerHTML 
-}
-
-function removeStyle(element) {
-  element.classList.remove("crossOut");
-  const icons = element.getElementsByClassName('fa-check')
-  icons[0].remove()
-  
-}
-
-let isCrossOut = false
-function toggleStyle(param) {
-  if (isCrossOut) {
-    removeStyle(param)
-    isCrossOut = false
+function toggleStyle(listItem) {
+  const toggle = listItem.classList.toggle("crossOut");
+  if (toggle) {
+    const checkIcon = document.createElement("i");
+    checkIcon.classList.add("fa", "fa-check");
+    listItem.append(checkIcon)
   } else {
-    addStyle(param)
-    isCrossOut = true
+    const checkIcon = listItem.getElementsByClassName('fa-check')[0]
+    checkIcon.remove()
   }
-
 }
-
 
 // eslint-disable-next-line no-unused-vars
 function insertListItem() {
   if (!newItem) return alert("¡Invalid text!");
   // Create list item and add current value from input
   const li = document.createElement("li");
-  const span = document.createElement("span")
-  span.innerText= newItem;
+  const span = document.createElement("span");
+  span.innerText = newItem;
 
   li.append(span);
   // Create and add button to list item
   createBtn(li);
   list.append(li);
- 
+
   // Reset values
   text.value = "";
   newItem = "";
 
-  li.addEventListener("click", () => toggleStyle(li))
-
+  li.addEventListener("click", () => toggleStyle(li));
 }
-
-
-
